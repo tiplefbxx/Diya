e37cdd5174c1a3c44844cba7b29ebad88cee9483
Maxim Pozichenko <valdema1999@ukr.net>
2021-11-21 17:11:27 +0200

    split into component

--- src/js_modules/content/content.js
+++ src/js_modules/content/content.js
@@ -1,24 +1,13 @@
 import './content.scss'
-import warning from '../../texure/cards/warning.png'
+import { Sertificate } from './sertificate/sertificate'
+import { Passport } from './passport/passport'
 
 export const Content = (props) => {
 
  return (
   <main className="content">
       <div className="content__line">
-          <div className="card">
-             <div className="card__wrapper">
-                 <div className="card__top">
-                     <div className="card__warning"><img src={warning} alt={''}></img></div>
-                     <div className="card__text">У вас ще немає <br/> COVID-сертифікатів</div>
-                     <div className="card__smalltext">Додайте жовтий або зелений <br/> сертифікат, якщо вакцинувалися.</div>
-                 </div>
-                 <div className="card__bottom">
-                     <div className="card__button">+</div>
-                     <div className="card__text">Додати <br/> COVID-сертифікат</div>
-                 </div>
-             </div>
-          </div>
+          <Passport />
       </div>
   </main>
  )
